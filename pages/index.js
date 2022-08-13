9b6c54852c870746e18f00b11aab0012c852028c
timiodubola <odubolaoluwatimilehin@gmail.com>
2022-08-14 00:30:53 +0100

    phase header component done

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import PageWrapper from '../components/page-wrapper/page-wrapper.comp';
+import PhaseHeader from '../components/phase-header/phase-header.comp';
+import { StyledHomePage } from '../styles/pages/home.styled';
 
 export default function Home() {
   return (
-    <PageWrapper>
-      <h1>Home page</h1>
+    <PageWrapper title='My startup progress'>
+      <StyledHomePage>
+        <h1>My startup progress</h1>
+
+        <PhaseHeader title='Foundation' progressNumber={1} isCompleted={true} />
+      </StyledHomePage>
     </PageWrapper>
   );
 }
